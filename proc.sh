f822bc7b0ad3c9c86dcf1f5d696f3bb42cf9660e
skotbela <skotbela@gmail.com>
2021-04-14 14:55:04 +0200

    process calculation updated - number of lines -1 becouse of the header line

--- proc.sh
+++ proc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-process_number=$(ps axu | wc -l)
+line_number=$(ps axu | wc -l) # The command wc provides lineCount (-l), wordCount (-w), and charCount (-c)
+process_number=$((line_number-1))
 echo "Number of running processes: $process_number"
 
 
