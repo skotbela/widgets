98eca9d0cbb227b474483d37d7adb5da9b4415fe
blippard <blippard@gmail.com>
2021-04-14 13:26:23 +0200

    Edit: created main function for widgets.sh

--- widgets.sh
+++ widgets.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-if [ -z ${WIDGET_CONTROL+x} ]; then
-  ./mem.sh
-  ./cpu.sh
-else
-  select_widget
-fi
+main() {
+  if [ -z ${WIDGET_CONTROL+x} ]; then
+    ./mem.sh
+    ./cpu.sh
+  else
+    select_widget
+  fi
+}
 
-select_widget(){
-  i=1;
+select_widget() {
+  i=1
   while [ "$i" -lt 6 ]; do
     file_var=$(echo "${WIDGET_CONTROL}" | cut -d',' -f"$i")
-    if [[ -z "$file_var" ]]
-      then
-        echo "";
-        tput cuu1
-        tput el
-      else
-        source ./$file_var.sh
+    if [[ -z "$file_var" ]]; then
+      echo ""
+      tput cuu1
+      tput el
+    else
+      source ./$file_var.sh
     fi
-    i=$((i + 1));
+    i=$((i + 1))
   done
 }
+
+main
