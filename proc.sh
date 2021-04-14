a383d5916ec61138448cf7f5eb2b7f5d22b791fc
blippard <blippard@gmail.com>
2021-04-14 15:05:43 +0200

    Edit: created visual and casual functions to display in window or in terminal

--- proc.sh
+++ proc.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-process_number=$(ps axu | wc -l)
-echo "Number of running processes: $process_number"
+main() {
+  option=$1
+  if [[ "$option" == "visual" ]]
+  then
+    run_visual;
+  else
+    run_casual;
+  fi
+}
 
+run_casual() {
+  process_number=$(ps axu | wc -l)
+  echo "Number of running processes: $process_number"
+}
 
+run_visual() {
+  zenity \
+    --info \
+    --text="<span size=\"xx-large\">$(ps axu | wc -l).</span>" \
+    --title="Memory-Widget" \
+    --ok-label="OK"
+}
+
+main "$@"
