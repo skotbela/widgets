39848247b2d8c9d9c355a39096d6a6965695266f
blippard <blippard@gmail.com>
2021-04-14 13:18:52 +0200

    Edit: added while loop to iterate through WIDGET_CONTROL variables and start relevant widget

--- widgets.sh
+++ widgets.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-./mem.sh
-./proc.sh
-./user.sh
-./disk.sh
+#./mem.sh
+#./proc.sh
+#./user.sh
+#./disk.sh
 
-if [ -z ${WIDGET_CONTROL+x} ];
-  then
-    ./mem.sh
-    ./cpu.sh
-  else
-    echo "var is set to '$WIDGET_CONTROL'";
+if [ -z ${WIDGET_CONTROL+x} ]; then
+  ./mem.sh
+  ./cpu.sh
+else
+  echo "TODO // add function"
 fi
+
+i=0;
+while [ "$i" -lt 6 ]; do
+  file_var=$(echo "${WIDGET_CONTROL}" | cut -d',' -f"$i")
+  source ./$file_var.sh
+  i=$((i + 1));
+done
