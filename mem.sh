bf4e2d77937b34d78583844a3bddb7f074a4a6a1
blippard <blippard@gmail.com>
2021-04-14 15:20:37 +0200

    Refactor: fixed typo

--- mem.sh
+++ mem.sh
@@ -12,13 +12,18 @@ main() {
 }
 
 run_casual() {
+  echo "---== MEMORY DATA ==---
+  "
+
   grep -E "MemFree|MemTotal" /proc/meminfo
+
+  echo ""
 }
 
 run_visual() {
   zenity \
   --info \
-  --text="<span size=\"xx-large\">$(grep -E "MemFree|MemTotal" /proc/meminfo).</span>" \
+  --text="<span size=\"xx-large\">$(grep -E "MemFree|MemTotal" /proc/meminfo)</span>" \
   --title="Memory-Widget" \
   --ok-label="OK"
 }
