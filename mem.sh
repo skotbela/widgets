0c7ea639530aceb45cce9a0f57c972c69dca2889
blippard <blippard@gmail.com>
2021-04-14 15:04:56 +0200

    Edit: created visual and casual functions to display in window or in terminal

--- mem.sh
+++ mem.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-grep -E "MemFree|MemTotal" /proc/meminfo
+
+main() {
+  option=$1
+  if [[ "$option" == "visual" ]]
+  then
+    run_visual;
+  else
+    run_casual;
+  fi
+}
+
+run_casual() {
+  grep -E "MemFree|MemTotal" /proc/meminfo
+}
+
+run_visual() {
+  zenity \
+  --info \
+  --text="<span size=\"xx-large\">$(grep -E "MemFree|MemTotal" /proc/meminfo).</span>" \
+  --title="Memory-Widget" \
+  --ok-label="OK"
+}
+
+main "$@"
