7c82130f05c3c4aef1b3fc52464076294251d563
skotbela <skotbela@gmail.com>
2021-04-14 14:00:40 +0200

    list logged-in users, all users, all groups

--- user.sh
+++ user.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+currently_logged_in_users=$(who | awk '{ print $1 }')
+echo "Currently logged in users: "
+echo $currently_logged_in_users
+
+all_users=$(getent passwd |awk -F':' '{ print $1}')
+echo "All users: "
+echo $all_users
+
+all_groups=$(getent group |awk -F':' '{ print $1}')
+echo "All groups: "
+echo $all_groups
+
